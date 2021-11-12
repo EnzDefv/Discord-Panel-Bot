2c966bb1a1759c990115cd873831fd205e5879a6
EnzDefv <94133670+EnzDefv@users.noreply.github.com>
2021-11-12 22:27:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const express = require('express')
+const discord = require('./bot')
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const fileUpload = require('express-fileupload');
+const config = require('./config/config.json')
 
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
